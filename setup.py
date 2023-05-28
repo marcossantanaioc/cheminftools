d8400cd4dfce3475043293eafc270c848b7010b4
marcossantanaioc <marcosvssantana@gmail.com>
2023-05-28 17:46:37 +0100

    Overhaul

--- setup.py
+++ setup.py
@@ -48,10 +48,6 @@ setuptools.setup(
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
     zip_safe = False,
-    entry_points = {
-        'console_scripts': cfg.get('console_scripts','').split(),
-        'nbdev': [f'{cfg.get("lib_path")}={cfg.get("lib_path")}._modidx:d']
-    },
     **setup_cfg)
 
 
