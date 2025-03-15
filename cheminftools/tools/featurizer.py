e858476de78b3a04b21983da43b5b786e49399d2
mvssantana-dev <mvssantanadev@gmail.com>
2025-03-15 21:44:56 +0000

    Updated dependencies

--- cheminftools/tools/featurizer.py
+++ cheminftools/tools/featurizer.py
@@ -1,4 +1,4 @@
-__all__ = ['MolFeaturizer']
+__all__ = ["MolFeaturizer"]
 
 import numpy as np
 from rdkit import Chem
@@ -27,31 +27,31 @@ class MolFeaturizer:
     generator
         A fingerprinter generator available in ´rdFingerprintGenerator´
 
-          """
+    """
 
     def __init__(self, descriptor_type: str, params: dict = {}):
-
         self.params = params
         self.descriptor_type = descriptor_type
 
-        self.DESCS = {'morgan': rdFingerprintGenerator.GetMorganGenerator,
-                      'atom_pairs': rdFingerprintGenerator.GetAtomPairGenerator,
-                      'rdkit': rdFingerprintGenerator.GetRDKitFPGenerator,
-                      'rdkit2d': self.get_rdkit2d_descriptors,
-                      'torsion': rdFingerprintGenerator.GetTopologicalTorsionGenerator,
-                      'maccs': MACCSkeys.GenMACCSKeys,
-                      'erg': GetErGFingerprint}
+        self.DESCS = {
+            "morgan": rdFingerprintGenerator.GetMorganGenerator,
+            "atom_pairs": rdFingerprintGenerator.GetAtomPairGenerator,
+            "rdkit": rdFingerprintGenerator.GetRDKitFPGenerator,
+            "rdkit2d": self.get_rdkit2d_descriptors,
+            "torsion": rdFingerprintGenerator.GetTopologicalTorsionGenerator,
+            "maccs": MACCSkeys.GenMACCSKeys,
+            "erg": GetErGFingerprint,
+        }
 
-        if descriptor_type in ['morgan', 'atom_pairs', 'rdkit', 'torsion']:
+        if descriptor_type in ["morgan", "atom_pairs", "rdkit", "torsion"]:
             self.generator = self.set_params(self.DESCS[descriptor_type], params)
         else:
             self.generator = self.DESCS[descriptor_type]
 
     def set_params(self, generator, params: dict):
-
         """
         Set parameters ´params´ for ´generator´
-        
+
         """
 
         try:
@@ -59,18 +59,18 @@ class MolFeaturizer:
 
         except ArgumentError:
             print(
-                f'The parameters {params} are not valid for generator {self.DESCS[self.descriptor_type].__name__}.'
-                f'\nSee RDKit: https://www.rdkit.org/docs/source/rdkit.Chem.rdFingerprintGenerator.html')
-            print('Returning the generator with default parameters.')
+                f"The parameters {params} are not valid for generator {self.DESCS[self.descriptor_type].__name__}."
+                f"\nSee RDKit: https://www.rdkit.org/docs/source/rdkit.Chem.rdFingerprintGenerator.html"
+            )
+            print("Returning the generator with default parameters.")
             generator = generator()
 
         return generator
 
     def transform_one(self, smi: str, use_counts: bool = False) -> np.array:
-
         """
         Generate features for one SMILES.
-        
+
         Arguments
         ---------
         smi
@@ -84,19 +84,19 @@ class MolFeaturizer:
         if not mol:
             return None
 
-        if self.descriptor_type == 'erg':
+        if self.descriptor_type == "erg":
             return self.generator(mol).reshape(1, -1)
 
-        if self.descriptor_type == 'maccs':
+        if self.descriptor_type == "maccs":
             fps = np.array([])
             ConvertToNumpyArray(self.generator(mol), fps)
             return fps.reshape(1, -1)
 
-        elif self.descriptor_type == 'rdkit2d':
+        elif self.descriptor_type == "rdkit2d":
             return self.generator(mol)
 
         else:
-            if self.descriptor_type == 'morgan' and use_counts:
+            if self.descriptor_type == "morgan" and use_counts:
                 fps = self.generator.GetCountFingerprintAsNumPy(mol)
                 return fps.reshape(1, -1)
 
@@ -104,22 +104,21 @@ class MolFeaturizer:
             return fps.reshape(1, -1)
 
     def transform(self, smiles_list: List[str], use_counts: bool = False) -> np.array:
-
         """
         Generate features for a list of SMILES.
-        
+
         Arguments
-        ---------   
+        ---------
         use_counts
             Whether to use count during fingerprint calculation.
         smiles_list
             A list of SMILES.
-            
+
         Returns
-        -------  
+        -------
         fps
             A fingerprint array.
-                                    
+
         """
 
         func = partial(self.transform_one, use_counts=use_counts)
@@ -130,7 +129,6 @@ class MolFeaturizer:
         return fps[-1]
 
     def get_rdkit2d_descriptors(self, mol: Chem.rdchem.Mol):
-
         """
         Generates 200 RDKit constitutional descriptors for a `mol` object.
 
@@ -147,5 +145,7 @@ class MolFeaturizer:
 
         """
         descriptor_dict = {name: func for name, func in Descriptors.descList}
-        descs = np.array([func(mol) for name, func in descriptor_dict.items()]).reshape(1, -1)
+        descs = np.array([func(mol) for name, func in descriptor_dict.items()]).reshape(
+            1, -1
+        )
         return descs
